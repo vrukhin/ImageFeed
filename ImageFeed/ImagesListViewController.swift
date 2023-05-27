94355d5bb6ec887b8fb40bafdf30894c636fc5c9
Pavel Vrukhin <vrukhin@protonmail.com>
2023-05-28 02:40:51 +0300

    ImagesListViewController подписан на протоклы DtaSource и Delegate

---
+++ ImageFeed/ImagesListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Павел Врухин on 26.05.2023.
+//
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+
+    @IBOutlet private var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+
+
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
+}
