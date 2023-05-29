205d78b485bad4dd19169624963df9a2cf2a60c3
Pavel Vrukhin <vrukhin@protonmail.com>
2023-05-29 19:40:41 +0300

    Установлены лайки четным фото вместо нечетных

--- Image Feed/ImagesList/ImagesListViewController.swift
+++ Image Feed/ImagesList/ImagesListViewController.swift
@@ -45,7 +45,7 @@ extension ImagesListViewController {
         cell.cellImage.image = image
         cell.dateLabel.text = dateFormatter.string(from: Date())
 
-        let isLiked = indexPath.row % 2 == 0
+        let isLiked = indexPath.row % 2 != 0
         let likeImage = isLiked ? UIImage(named: "like_button_on") : UIImage(named: "like_button_off")
         cell.likeButton.setImage(likeImage, for: .normal)
     }
