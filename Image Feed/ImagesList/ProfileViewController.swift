dc54ccba33f2dc11879e4b94fae72bc94497127f
Pavel Vrukhin <vrukhin@protonmail.com>
2023-06-09 23:15:27 +0300

    Сверстан экран профиля

---
+++ Image Feed/ImagesList/ProfileViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileViewController.swift
+//  Image Feed
+//
+//  Created by Павел Врухин on 09.06.2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+
+    @IBOutlet private weak var avatarImage: UIImageView!
+    @IBOutlet private weak var exitButton: UIButton!
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var usernameLabel: UILabel!
+    @IBOutlet private weak var textLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        exitButton.titleLabel?.text = ""
+    }
+}
