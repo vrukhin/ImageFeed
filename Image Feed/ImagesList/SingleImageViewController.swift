bddc35112c741e8a2264dd314b888e22a1806539
Pavel Vrukhin <vrukhin@protonmail.com>
2023-06-10 08:07:06 +0300

    Сверстан экран просмотра картинки

---
+++ Image Feed/ImagesList/SingleImageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SingleImageViewController.swift
+//  Image Feed
+//
+//  Created by Павел Врухин on 10.06.2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+
+    @IBOutlet private weak var imageView: UIImageView!
+}
