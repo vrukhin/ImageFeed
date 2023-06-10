20e70f49c94b1091fe7b152f69d437d94e07d185
Pavel Vrukhin <vrukhin@protonmail.com>
2023-06-10 16:30:07 +0300

    Реализовано центрирование изображения в Single Image View

--- Image Feed/ImagesList/SingleImageViewController.swift
+++ Image Feed/ImagesList/SingleImageViewController.swift
@@ -23,6 +23,24 @@ final class SingleImageViewController: UIViewController {
         imageView.image = image
         scrollView.minimumZoomScale = 0.1
         scrollView.maximumZoomScale = 1.25
+        rescaleAndCenterImageInScrollView(image: image)
+    }
+    
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
     }
 }
 
