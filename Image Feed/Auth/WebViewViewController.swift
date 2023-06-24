632a372ee1ece816c10c845f490eb1549f42a96c
Pavel Vrukhin <vrukhin@protonmail.com>
2023-06-25 00:08:15 +0300

    Верстка WebView

---
+++ Image Feed/Auth/WebViewViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WebViewViewController.swift
+//  Image Feed
+//
+//  Created by Павел Врухин on 24.06.2023.
+//
+
+import WebKit
+
+class WebViewViewController: UIViewController {
+    @IBOutlet private var webView: WKWebView!
+    @IBAction private func didTapBackButton(_ sender: Any?) {}
+}
