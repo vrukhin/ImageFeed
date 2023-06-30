6105b635b106b138be8008326f9d2444164a67ea
Pavel Vrukhin <vrukhin@protonmail.com>
2023-07-01 01:08:59 +0300

    Добавлен файл с константами

---
+++ Image Feed/Constants.swift
@@ -0,0 +1,5 @@
+let UnsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+let AccessKey = "XpPomELrVMrC-Nq2rveKG6hdHbH0rMkQnVN4SF5M5vc"
+let SecretKey = "lLIDFsCxLBx3_WnRiydhODcF5f9IcqtQ1MQ-IOKDfIk"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
