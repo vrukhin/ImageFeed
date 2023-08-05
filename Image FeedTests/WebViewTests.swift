0a2bcfa2376f24ae216d2dcf131c76bd35d84569
Pavel Vrukhin <vrukhin@protonmail.com>
2023-08-05 20:26:38 +0300

    Тест связи контроллера и презентера

---
+++ Image FeedTests/WebViewTests.swift
@@ -0,0 +1,41 @@
+//
+//  Image_FeedTests.swift
+//  Image FeedTests
+//
+//  Created by Павел Врухин on 05.08.2023.
+//
+
+@testable import Image_Feed
+import XCTest
+
+final class WebViewPresenterSpy: WebViewPresenterProtocol {
+    var viewDidLoadCalled: Bool = false
+    var view: WebViewViewControllerProtocol?
+    
+    func viewDidLoad() {
+        viewDidLoadCalled = true
+    }
+    
+    func didUpdateProgressValue(_ newValue: Double) {}
+    
+    func code(from url: URL) -> String? {
+        return nil
+    }
+}
+
+final class WebViewTests: XCTestCase {
+    func testViewControllerCallsViewDidLoad() {
+        //given
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(identifier: "WebViewViewController") as! WebViewViewController
+        let presenter = WebViewPresenterSpy()
+        viewController.presenter = presenter
+        presenter.view = viewController
+        
+        //when
+        _ = viewController.view
+        
+        //then
+        XCTAssertTrue(presenter.viewDidLoadCalled)
+    }
+}
