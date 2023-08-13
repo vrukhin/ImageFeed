621fa88b33e53e2bd874f3a3412182713aeedb98
Pavel Vrukhin <vrukhin@protonmail.com>
2023-08-13 23:02:29 +0300

    Добавлен UI-тест авторизации и загрузки ленты

--- Image FeedUITests/Image_FeedUITests.swift
+++ Image FeedUITests/Image_FeedUITests.swift
@@ -17,7 +17,31 @@ final class Image_FeedUITests: XCTestCase {
     }
     
     func testAuth() throws {
+        app.buttons["Authenticate"].tap()
+        let webView = app.webViews["UnsplashWebView"]
         
+        XCTAssertTrue(webView.waitForExistence(timeout: 5))
+
+        let loginTextField = webView.descendants(matching: .textField).element
+        XCTAssertTrue(loginTextField.waitForExistence(timeout: 5))
+        
+        loginTextField.tap()
+        loginTextField.typeText("")
+        webView.swipeUp()
+        
+        let passwordTextField = webView.descendants(matching: .secureTextField).element
+        XCTAssertTrue(passwordTextField.waitForExistence(timeout: 5))
+        
+        passwordTextField.tap()
+        passwordTextField.typeText("")
+        webView.swipeUp()
+        
+        webView.buttons["Login"].tap()
+        
+        let tablesQuery = app.tables
+        let cell = tablesQuery.children(matching: .cell).element(boundBy: 0)
+        
+        XCTAssertTrue(cell.waitForExistence(timeout: 5))
     }
     
     func testFeed() throws {
