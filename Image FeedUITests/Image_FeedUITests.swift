56d8095c348c6249d29074f33f805ddb1e173e67
Pavel Vrukhin <vrukhin@protonmail.com>
2023-08-13 17:31:35 +0300

    Добавлен бандл с UI-тестами

---
+++ Image FeedUITests/Image_FeedUITests.swift
@@ -0,0 +1,30 @@
+//
+//  Image_FeedUITests.swift
+//  Image FeedUITests
+//
+//  Created by Павел Врухин on 13.08.2023.
+//
+
+import XCTest
+
+final class Image_FeedUITests: XCTestCase {
+    
+    private let app = XCUIApplication()
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+        app.launch()
+    }
+    
+    func testAuth() throws {
+        
+    }
+    
+    func testFeed() throws {
+        
+    }
+    
+    func testProfile() throws {
+        
+    }
+}
