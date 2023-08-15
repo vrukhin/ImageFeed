b903489846d0296789de62f42d1b2f6dbdb9758b
Pavel Vrukhin <vrukhin@protonmail.com>
2023-08-16 00:01:35 +0300

    Добавлены UI-тесты ленты и профиля

--- Image FeedUITests/Image_FeedUITests.swift
+++ Image FeedUITests/Image_FeedUITests.swift
@@ -45,10 +45,43 @@ final class Image_FeedUITests: XCTestCase {
     }
     
     func testFeed() throws {
+        let tablesQuery = app.tables
         
+        let cell = tablesQuery.children(matching: .cell).element(boundBy: 0)
+        cell.swipeUp()
+        
+        sleep(2)
+        
+        let cellToLike = tablesQuery.children(matching: .cell).element(boundBy: 1)
+        
+        cellToLike.buttons["like button off"].tap()
+        cellToLike.buttons["like button on"].tap()
+        
+        sleep(2)
+        
+        cellToLike.tap()
+        
+        sleep(2)
+        
+        let image = app.scrollViews.images.element(boundBy: 0)
+        // Zoom in
+        image.pinch(withScale: 3, velocity: 1) // zoom in
+        // Zoom out
+        image.pinch(withScale: 0.5, velocity: -1)
+        
+        let navBackButtonWhiteButton = app.buttons["nav back button white"]
+        navBackButtonWhiteButton.tap()
     }
     
     func testProfile() throws {
+        sleep(3)
+        app.tabBars.buttons.element(boundBy: 1).tap()
+       
+        XCTAssertTrue(app.staticTexts["Name Lastname"].exists)
+        XCTAssertTrue(app.staticTexts["@username"].exists)
         
+        app.buttons["logout button"].tap()
+        
+        app.alerts["Bye bye!"].scrollViews.otherElements.buttons["Yes"].tap()
     }
 }
